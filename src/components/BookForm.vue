a26da851d161b465edcfd68601128b910c7dca3c
Jason Houle <jhoule@connecticutchildrens.org>
2019-02-15 08:36:49 -0500

    module 5

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="" action="#" method="post" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+    <input type="text" name="title" v-model="bookTitle" value="" placeholder="Book Title">
+    <input type="text" name="author" v-model="bookAuthor" value="" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'BookForm',
+    props: ['books'],
+    data: function() {
+      return {
+        bookTitle: '',
+        bookAuthor: '',
+      }
+    },
+    methods: {
+      bookSubmit: function(bookTitle, bookAuthor) {
+        this.$emit('addBook', bookTitle, bookAuthor);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
